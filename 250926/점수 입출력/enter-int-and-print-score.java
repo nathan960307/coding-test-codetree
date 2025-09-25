da6a3b796b1284de1682c5da06ce5708e8424710
nathan <168330519+nathan960307@users.noreply.github.com>
2025-09-26 01:22:37 +0900

    Title: 점수 입출력, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250926/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        // Scanner 객체 생성 후 사용 예시
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next(); 
+        System.out.printf("Your score is %s point.",a);
+
+
+    }
+}
