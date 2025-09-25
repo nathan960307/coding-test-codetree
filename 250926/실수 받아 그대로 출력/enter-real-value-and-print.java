200ce324a8c658b7987c651ec0fdda26df871415
nathan <168330519+nathan960307@users.noreply.github.com>
2025-09-26 01:25:14 +0900

    Title: 실수 받아 그대로 출력, Time: 148ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250926/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        // Scanner 객체 생성 후 사용 예시
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble(); 
+        System.out.printf("%.2f",a);
+    }
+}
