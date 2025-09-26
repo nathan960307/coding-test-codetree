a2747d59d27a02a1e5372f7c3025a94d6ebcdeb1
nathan <168330519+nathan960307@users.noreply.github.com>
2025-09-26 15:41:28 +0900

    Title: 등급 매기기, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250926/등급 매기기/ranking.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int num1 = sc.nextInt();
+        
+        
+        if(num1 >= 90){
+            System.out.print("A");
+        }else if(num1 >=80){
+            System.out.print("B");
+        }else if(num1 >=70){
+            System.out.print("C");
+        }else if(num1 >=60){
+            System.out.print("D");
+        }else{
+            System.out.print("F");
+        }
+
+    
+
+        
+    }
+}
