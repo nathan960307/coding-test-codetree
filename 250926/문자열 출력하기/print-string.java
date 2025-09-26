3a4f11076b211de9c002ae66700e81ec5f0ac646
nathan <168330519+nathan960307@users.noreply.github.com>
2025-09-26 21:02:11 +0900

    Title: 문자열 출력하기, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250926/문자열 출력하기/print-string.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+        
+        int num1 = sc.nextInt();
+        for (int i = 0; i < num1; i++) {
+            System.out.println("LeebrosCode");
+        }
+    }
+}
