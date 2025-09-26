3bee4df6a754d1afb98d5b452690ac079414db95
nathan <168330519+nathan960307@users.noreply.github.com>
2025-09-26 21:39:06 +0900

    Title: 점수 비교, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250926/점수 비교/score-comparison.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+        
+        int math1 = sc.nextInt();
+        int math2 = sc.nextInt();
+        int eng1 = sc.nextInt();
+        int eng2 = sc.nextInt();
+
+        if(math1 > math2 && eng1 > eng2){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+    }
+}
