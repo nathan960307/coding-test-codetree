ef21cf381c6fa4d3ea188ebac02ab0b9578b6d25
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-10 21:27:52 +0900

    Title: 친근하지 않은 수, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251210/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int answer = 0;
+
+        for(int i=1; i<N; i++){
+            if(i%2==0 || i%3==0 || i%5==0){
+                continue;
+            }
+            answer +=1;
+        }
+        System.out.println(answer);
+    }
+}
