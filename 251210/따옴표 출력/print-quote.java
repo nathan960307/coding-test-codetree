b0e7d1c42c9b38ea251de5e366e4b6f833db0dc7
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-10 20:49:30 +0900

    Title: 따옴표 출력, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251210/따옴표 출력/print-quote.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        String a = "He says \"It\'s a really simple sentence\".";
+
+        System.out.println(a);
+    }
+}
