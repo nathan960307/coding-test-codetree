9eb6f1f03a8ea30cfcc06c2d36a3ba318eca61a1
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-11 15:27:12 +0900

    Title: 숫자 피라미드, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251211/숫자 피라미드/number-pyramid.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+         Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        
+        for(int i=1; i<=a; i++){
+            for(int j=1; j<=a; j++){
+                System.out.print("*"); 
+            }
+            System.out.println("");
+        }
+            
+    }
+}
