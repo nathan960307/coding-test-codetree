98e4bb79c087009d76073923b7f3f2450485fa2a
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-13 06:02:36 +0900

    Title: 날짜 변경하여 출력, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

--- 251213/날짜 변경하여 출력/print-date-with-different-format.java
+++ 251213/날짜 변경하여 출력/print-date-with-different-format.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main (String args[]) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        String[] strArr = s.split(".");
-        System.out.println(strArr[1] + " - " + strArr[2] + " - " + strArr[0]);
+        String[] strArr = s.split("\\."); // . 은 정규표현식으로 처리됨으로 이스케이프 문자 사용 필요
+        System.out.println(strArr[1] + "-" + strArr[2] + "-" + strArr[0]);
     }
 }
