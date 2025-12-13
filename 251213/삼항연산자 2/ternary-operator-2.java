7c0406f33dd78f3764200ef7d4e73f8fb0b5f3dc
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-13 14:53:17 +0900

    Title: 삼항연산자 2, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251213/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+        int a = sc.nextInt();
+       
+
+        char answer = a == 1 ? 't' : 'f';
+
+        System.out.println(answer);
+    }
+}
