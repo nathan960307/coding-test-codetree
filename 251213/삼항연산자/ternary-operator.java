f90c9433989cefdf6e095abadcd45476ebbbcf58
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-13 14:52:14 +0900

    Title: 삼항연산자, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251213/삼항연산자/ternary-operator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+        int a = sc.nextInt();
+       
+
+        String pass = a == 100 ? "pass" : "failure";
+
+        System.out.println(pass);
+    }
+}
