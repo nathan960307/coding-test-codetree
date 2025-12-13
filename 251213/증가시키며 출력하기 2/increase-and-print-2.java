d9a6e145ea8d0f41aa44b853d9fef003141070ca
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-13 17:44:00 +0900

    Title: 증가시키며 출력하기 2, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251213/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        
+        int i = 10;
+        
+        while(i<= 26){
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
