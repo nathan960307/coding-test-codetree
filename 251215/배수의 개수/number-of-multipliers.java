919c918450d8584bfaf01501b3fb59354cb44892
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-15 13:23:29 +0900

    Title: 배수의 개수, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 251215/배수의 개수/number-of-multipliers.java
+++ 251215/배수의 개수/number-of-multipliers.java
@@ -4,16 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int count = 0;
+        int count3 = 0;
+        int count5 = 0;
         for (int i = 1; i <= 10; i++) {
             int n = sc.nextInt();
 
-            if(n % 3 ==0 && n % 5 ==0){
-                count++;
+            if(n % 3 ==0){
+                count3++;
+            }
+            if(n % 5 ==0){
+                count5++;
             }
             
         }
 
-        System.out.print(count);
+        System.out.print(count3 + " " +count5 );
     }
 }
