f3a709abf65c83b8556847d9a41d07c1c6808dbd
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-15 17:34:38 +0900

    Title: A의 B승, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251215/A의 B승/a-to-the-power-of-b.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int sum = 0;
+        int count = 0;
+        int prod = 1;
+        double avg = 0;
+        
+        int max = a>b? a : b;
+        int min = a<b? a : b;
+
+
+        for (int i = 1; i <= b; i++) {
+            prod = prod * a;
+        }
+
+        System.out.printf("%d",prod);
+    }
+}
