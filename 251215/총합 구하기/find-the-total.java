5af4f43f4d9f8f6bdc6b889d1498f99017a24232
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-15 17:25:58 +0900

    Title: 총합 구하기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251215/총합 구하기/find-the-total.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int sum = 0;
+        int count = 0;
+        double avg = 0;
+        
+        int max = a>b? a : b;
+        int min = a<b? a : b;
+
+
+        for (int i = min; i <= max; i++) {
+            if(i % 6==0 && i % 8 !=0){
+                sum = sum + i;
+                
+            }
+        }
+
+        
+        
+        System.out.printf("%d",sum);
+    }
+}
