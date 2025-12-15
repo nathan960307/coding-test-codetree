8ca92e6fbb9ed8d8d1081994075039839dc36341
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-15 17:58:42 +0900

    Title: 특정 조건의 온전수 구하기, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

--- 251215/특정 조건의 온전수 구하기/print-onjeonsu.java
+++ 251215/특정 조건의 온전수 구하기/print-onjeonsu.java
@@ -8,7 +8,7 @@ public class Main {
         int sum = 0;
         int count = 0;
         int prod = 1;
-        int ones = ;
+        int ones = 0;
 
         for (int i = 1; i <= n; i++) {
             ones = i %10;
