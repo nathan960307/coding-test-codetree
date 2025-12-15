64b83dde03bb1572ac58c85ae1c13e2a044cdcd0
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-15 17:49:56 +0900

    Title: 친근하지 않은 수, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251215/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+        int count = 0;
+        int prod = 1;
+
+
+        for (int i = 1; i <= n; i++) {
+            if(i % 2==0 || i % 3 ==0 || i % 5 ==0){
+                continue;
+            }else{
+                count++;
+            }
+        }
+
+        
+        
+        System.out.printf("%d",count);
+    }
+}
