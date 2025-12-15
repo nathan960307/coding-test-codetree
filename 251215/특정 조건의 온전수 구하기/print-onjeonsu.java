c4f27aabe74ddd09374846ade59910cf45f6cd66
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-15 17:58:14 +0900

    Title: 특정 조건의 온전수 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251215/특정 조건의 온전수 구하기/print-onjeonsu.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+        int count = 0;
+        int prod = 1;
+        int ones = ;
+
+        for (int i = 1; i <= n; i++) {
+            ones = i %10;
+            if(i % 2==0 || ones == 5 || (i % 3 ==0 && i % 9 !=0)){
+                continue;
+            }else{
+                System.out.print(i + " ");
+            }
+        }
+
+        
+        
+        
+    }
+}
