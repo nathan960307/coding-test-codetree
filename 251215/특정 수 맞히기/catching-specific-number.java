bea8af2530fcc90bf95e9cba6523900aa748687e
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-15 18:52:36 +0900

    Title: 특정 수 맞히기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251215/특정 수 맞히기/catching-specific-number.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while(true){
+            int a = sc.nextInt();
+
+            if(a ==25){
+                System.out.println("Good");
+                break;
+            }else if(a < 25){
+                System.out.println("Higher");
+            }else if(a > 25){
+                System.out.println("Lower");
+            }
+            
+        }
+        
+    }
+}
