2bc0f2659ca472d195eacfc633717f0074290f33
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-16 22:47:45 +0900

    Title: 별표 출력하기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251216/별표 출력하기/print-star.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 1;
+
+        for(int i =1; i<=2*n-1; i++){ // 가로줄의 갯수
+            for(int j =0; j<count; j++){ // 세로줄의 갯수
+                System.out.print("* ");
+            }
+            System.out.println("");
+
+            if(i<n){
+                count++;
+            }else{
+                count--;
+            }
+        }
+
+        
+}
+}
