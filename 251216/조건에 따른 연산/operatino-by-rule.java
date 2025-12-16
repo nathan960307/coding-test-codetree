59c7a8e47813ac0084303b3f8184383e99966ce5
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-16 15:24:37 +0900

    Title: 조건에 따른 연산, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251216/조건에 따른 연산/operatino-by-rule.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+
+        int sum = 0;
+        int count =0;
+        double avg = 0.0;
+        int temp =2;
+
+        while(n<1000){
+            if(n%2==0){
+                n = (n*3)+1;
+            }else if(n%2!=0){
+                n = (n*2)+2;
+            }
+            count++;
+    }
+    System.out.println(count);
+}
+}
