a947efcbb02231629bfadf23b58184430ee460be
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-16 15:36:28 +0900

    Title: 합성수 판별기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251216/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+
+        int sum = 0;
+        int count =0;
+        double avg = 0.0;
+        int temp =2;
+        boolean is = false;
+
+        for(int i=1;i<=n;i++){
+            if(n / i == 0){
+                is = true;
+                break;
+            }
+        }
+        
+        if(is){
+            System.out.println("C");
+        }else{
+            System.out.println("N");
+        }
+        
+        
+}
+}
