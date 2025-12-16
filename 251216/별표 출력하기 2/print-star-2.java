ddce0e4eea7d9cbd645be0def5fac09f29f0e2cd
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-16 17:03:18 +0900

    Title: 별표 출력하기 2, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 251216/별표 출력하기 2/print-star-2.java
+++ 251216/별표 출력하기 2/print-star-2.java
@@ -7,7 +7,7 @@ public class Main {
         
         
         for(int i =0; i<n; i++){
-            for(int j =i; j>0; j--){
+            for(int j =n-i; j>0; j--){
                 System.out.print("* ");
             }
             System.out.println("");
