0b8bd7d000fad3651d114cb3108e88e7f5392564
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-16 22:49:35 +0900

    Title: 별표 출력하기 4, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

--- 251216/별표 출력하기 4/print-star-4.java
+++ 251216/별표 출력하기 4/print-star-4.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int count = 1;
+        int count = n;
 
         for(int i =1; i<=2*n-1; i++){ // 가로
             for(int j =0; j<count; j++){ // 세로
