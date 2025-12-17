a7fec2eb0c39bf429b094fa8637d0cba56454cde
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-17 22:03:17 +0900

    Title: 규칙에 맞는 출력, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251217/규칙에 맞는 출력/output-that-matches-the-rule.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for (int i = 1; i <= n; i++) { // 가로
+                for (int j = 1; j <=i; j++) { //세로 (별)
+                    System.out.print(n-i+j + " ");
+            
+                }
+
+            System.out.println(); 
+        }
+    }
+}
