b0054ac9361498afac60ff51c8ae69dba4ec87cd
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-17 14:05:14 +0900

    Title: 별표 출력하기 8, Time: 3041ms, Memory: 24MB, Status: Time Limit Exceed - Codetree

---
+++ 251217/별표 출력하기 8/print-star-8.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int star = 2;
+        int space = n;
+
+        for(int i =1; i<=n; i++){ // 가로
+
+            if(i%2!=0){
+                System.out.print("*");
+            }else{
+                for(int j =1; j<=star; j++){ // 세로(별)
+                System.out.print("* ");
+                star = star + 2;
+                }
+            }
+
+            
+
+            System.out.println("");
+
+
+            
+        }
+    
+}
+}
