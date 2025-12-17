a47c3c1edaafab988e947c654d0f47df2df0d306
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-17 15:55:51 +0900

    Title: 좌우반전시킨 정사각형, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251217/좌우반전시킨 정사각형/left-right-inverted-square.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+
+        for(int i =1; i<=n; i++){ // 가로
+            for(int j=1; j<=n; j++){ // 세로
+                System.out.printf("%d ",i*(n-j+1));
+            }
+            System.out.println("");
+        }
+    
+    }
+}
