558c122269ea9f333438ab96f918a81a92d14bec
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-17 23:20:03 +0900

    Title: 짝수의 합 n번 구하기, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251217/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        int count = 0;
+        
+        
+
+        for (int i=1; i<=n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            int sum =0;
+            int divisor = 0;
+            
+            for(int j=a; j<=b; j++){
+                if(j%2==0){
+                    sum = sum + j;
+                }
+            }        
+            System.out.println(sum);
+        }
+    }
+}
