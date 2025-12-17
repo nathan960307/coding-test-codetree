22545f41c6d3e9e40ab6ae40209cb4bcb3095f59
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-17 18:28:12 +0900

    Title: 1-9 정사각형, Time: 134ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251217/1-9 정사각형/1-9-square.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 1;
+        
+
+        for(int i =1; i<=n; i++){ // 가로
+            for(int j=1; j<=n; j++){ // 세로
+                System.out.printf("%d",count);
+                
+                if(count==9){
+                    count = 0;
+                }
+
+                count++;
+                
+            }
+            System.out.println("");
+        }
+    
+    }
+}
