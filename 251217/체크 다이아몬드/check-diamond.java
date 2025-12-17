b529fe90736dc6fc4e57ded2a3979c5018076ea7
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-17 13:59:03 +0900

    Title: 체크 다이아몬드, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251217/체크 다이아몬드/check-diamond.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int star = 1;
+        int space = n;
+
+        for(int i =1; i<=2*n-1; i++){ // 가로
+
+            for(int j =1; j<=space-1; j++){ // 세로(공백)
+                System.out.print(" ");
+            }
+
+            for(int j =1; j<=star; j++){ // 세로(별)
+                System.out.print("* ");
+            }
+
+            System.out.println("");
+
+
+            if(i<n){
+                space--;
+                star++;
+            }else{
+                space++;
+                star--;
+            }
+        }
+    
+}
+}
