bfe893d982136a309a881c2976127d7fe9080222
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-17 22:34:09 +0900

    Title: ab곱 N번 구하기, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251217/ab곱 N번 구하기/a-multiple-b-n-times.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int sum = 1;
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            for (int j = a; j <= b; j++) {
+                sum = sum * j;
+            }
+
+            System.out.println(sum);
+            sum = 1;
+        }
+    }
+}
