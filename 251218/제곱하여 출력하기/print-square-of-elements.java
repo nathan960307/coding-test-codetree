8036c26c2344645b6d8755166a31c20f234ffe08
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-18 16:55:58 +0900

    Title: 제곱하여 출력하기, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 251218/제곱하여 출력하기/print-square-of-elements.java
+++ 251218/제곱하여 출력하기/print-square-of-elements.java
@@ -10,7 +10,7 @@ public class Main {
         int index = 0;
         int odd_sum = 0;
         int even_sum = 0;
-        int[] arr = new int[10];
+        int[] arr = new int[n];
 
         for (int i = 0; i < n; i++) {
             // arr[i] = sc.next().charAt(0);
