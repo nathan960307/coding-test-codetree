8bd6492f941a15cfa0728fc70185797fa4b79c09
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-18 21:15:04 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 141ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251218/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int count = 0;
+        int answer = 0;
+        
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(arr[i]==2){
+                count++;
+            }
+
+            if(count == 3){
+                answer = i+1;
+                break;
+            }
+        }
+        
+        System.out.print(answer);
+
+    }
+}
