017c4ba185dbe541e24e9f220664b4b5fd972984
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-18 18:12:53 +0900

    Title: 점수대 파악하기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251218/점수대 파악하기/find-out-the-score-range.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int n = sc.nextInt();
+        
+        int[] arr = new int[100];
+        int index =0;
+
+        // for(int i=0; i< 100; i++){
+        //     arr[i] = sc.nextInt();
+        // }
+        while(true){
+            int temp = sc.nextInt();
+            if(temp==0){
+                break;
+            }
+            arr[index++] = temp;
+        }
+
+
+        for(int i=10; i>=1; i-=1){
+            int count =0;
+            for(int j=0; j<100;j++){
+
+                if(arr[j]/10==i){
+                    count++;
+                }
+            }
+            System.out.printf("%d - %d\n",i*10,count);
+        }
+    }
+}
