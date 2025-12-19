b1c547ba9c02af0c37217178ddfe223f4fd8355e
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-19 13:09:55 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 201ms, Memory: 12MB, Status: Passed - Codetree

--- 251219/중복되지 않는 정수 중 최대/max-of-unique-number.java
+++ 251219/중복되지 않는 정수 중 최대/max-of-unique-number.java
@@ -6,8 +6,6 @@ public class Main {
 
         int n = sc.nextInt(); // n 입력
         int[] arr = new int[n]; // n 크기의 배열 선언
-        int min = 1001;
-        int answer = -1;
 
         // n 크기의 배열 생성 및 입력 
         for(int i=0; i<n; i++){
@@ -25,29 +23,35 @@ public class Main {
             }
         }
 
-        // 첫번째 수와 두번째 수가 다른지 비교
+        
         if(n==1){
             System.out.print(arr[0]);
+            return;
         }
 
-        // int first = arr[0];
-        // int second = arr[1];
-        
-
-        if(arr[0] != arr[1] && n !=1){ // 첫번째 와 두번째가 다른 경우 
-            
-            // 정답 출력
+        // 맨 앞 체크
+        if (arr[0] != arr[1]) {
             System.out.print(arr[0]);
-
-        }else if(arr[0] == arr[1] && n !=1){ // 첫번째와 두번째가 같은 경우
-            for(int i=1; i<n-1; i++){
-                if(arr[i]!=arr[i-1] && arr[i] != arr[i+1]){
-                    answer = arr[i];
-                }else if(arr[i] == arr[i-1]){
-                    continue;
-                }
-            }
-            System.out.print(answer);
+            return;
         }
+
+        // 중간 체크
+        for (int i = 1; i < n - 1; i++) {
+            if (arr[i] != arr[i - 1] && arr[i] != arr[i + 1]) {
+                System.out.print(arr[i]);
+                return;
+            }
+        }
+
+        // 맨 뒤 체크
+        if (arr[n - 1] != arr[n - 2]) {
+            System.out.print(arr[n - 1]);
+            return;
+        }
+
+        // 유일한 값 없음
+        System.out.print(-1);
+             
+        
     }
 }
