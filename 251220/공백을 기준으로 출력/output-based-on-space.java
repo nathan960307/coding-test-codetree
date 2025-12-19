409643444ef64a77cbf2514f426b21f6a5bdb9b0
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-20 01:42:30 +0900

    Title: 공백을 기준으로 출력, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251220/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        String answer = str1 + str2;
+        answer = answer.replace(" ","");
+    
+        System.out.print(answer);
+    }
+}
+
+
