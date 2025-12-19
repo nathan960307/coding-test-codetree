29bfc92c1dd4d8ab8a3a729a43e3c8b5c84c995f
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-19 11:55:14 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 205ms, Memory: 13MB, Status: Passed - Codetree

---
+++ 251219/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int max = 0;
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[i] < arr[j]){
+                    if(max < arr[j]-arr[i] ){
+                        max = arr[j]-arr[i];
+                    }
+                }
+            }
+        }
+        
+        
+        
+        System.out.printf("%d",max);
+    }
+}
