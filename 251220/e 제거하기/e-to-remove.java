961cac24d02eebe1ff14095440fc9c67d2d22968
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-20 17:21:01 +0900

    Title: e 제거하기, Time: 108ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251220/e 제거하기/e-to-remove.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        
+
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='e'){
+                s = s.substring(0,i) + s.substring(i+1);
+                break;
+            }
+        }
+
+        System.out.print(s);
+      
+        
+    }
+}
+
+
+
+
