565673ec6cfb1b06a81f8ab2b13821eb672a6361
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-22 06:59:17 +0900

    Title: 문자열 거꾸로 출력하기, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251222/문자열 거꾸로 출력하기/print-string-backward.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+
+            String s = sc.next();
+
+            if(s.equals("END")){
+                break;
+            }
+
+            for(int i=s.length()-1; i>=0; i--){
+                char c = s.charAt(i);
+                System.out.print(c);
+            }
+            System.out.println("");
+
+
+        }
+
+
+    }
+}
+
+
+
+
