4a62bf93eed274fddb7b2cb4552b8d6f1e2449c3
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-22 17:10:22 +0900

    Title: 함수를 이용한 369 게임, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251222/함수를 이용한 369 게임/369-games-using-functions.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        // Please write your code here.
+        func1(A,B)
+    }
+
+    public static void func1(int a, int b) {
+        int count = 0;
+
+        for(int i=a; i<=b; i++){
+
+            if(i%3==0){
+                count++;
+            }
+
+            int temp = i;
+            while(temp>0){
+                
+                if(temp%10==3||temp%10==6||temp%10==9){
+                    count++;
+                    break;
+                }
+
+                temp = temp /10;
+                
+            }
+        }
+
+        System.out.println(count);
+
+        
+    }
+
+    
+}
