86b4ec3201a94051c90f34c4d6698de7082cdebd
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-24 18:12:10 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251224/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Please write your code here.
+        func1(a,b);
+    }
+
+    public static void func1(int a,int b) {
+        int count =0; 
+        
+        for(int i=a; i<=b; i++){
+            int num = i;
+            boolean is = true;
+            while(i>0){
+                int sum = i % 10; 
+                i = i/10;
+            }
+
+            for(int j=2; j<i){
+                if(i%j==0){
+                    is = true;
+                    break;
+                }
+            }
+
+            if(sum%2==0 && is == false){
+                count++
+            }
+        }
+        System.out.print(count);
+    }
+}
