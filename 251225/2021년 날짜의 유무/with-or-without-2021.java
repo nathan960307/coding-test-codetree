ca09d434efa6db6d742374e61a4af584b68cfdcf
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-25 15:01:20 +0900

    Title: 2021년 날짜의 유무, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 251225/2021년 날짜의 유무/with-or-without-2021.java
+++ 251225/2021년 날짜의 유무/with-or-without-2021.java
@@ -10,11 +10,11 @@ public class Main {
 
     public static void func1(int m,int d) {
         boolean is = false;
-        if(m==1 && m==3 && m==5 && m==7 && m==8 && m==10 && m==12){
+        if(m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12){
             if(d<=31){
                 is = true;
             }
-        }else if(m==4 && m==6 && m==9 && m==11){
+        }else if(m==4 || m==6 || m==9 || m==11){
             if(d<=30){
                 is = true;
             }
