9a9f15db8ad0a566186b93e7cf28e2aabfd7b0b6
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-27 02:50:00 +0900

    Title: 반복 출력하기 2, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251227/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        func1(n);
+    }
+    public static void func1(int n){
+        if(n==0){
+            return;
+        }
+        func1(n-1);
+        System.out.println("HelloWorld");
+    }
+}
