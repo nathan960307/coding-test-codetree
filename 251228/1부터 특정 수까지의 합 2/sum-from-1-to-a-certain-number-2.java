2dc4545f7c9e7dba64872b4d9c76e52599a264b7
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-28 04:02:47 +0900

    Title: 1부터 특정 수까지의 합 2, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

--- 251228/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.java
+++ 251228/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.java
@@ -8,17 +8,19 @@ public class Main {
         int n = sc.nextInt();
         // Please write your code here.
 
-        int answer = func1(n);
-        System.out.print(answer);
+        func1(n);
+        System.out.print(sum);
     }
 
-    public static int func1(int n){
-
-        sum = n;
-        func1(n-1);
-
-        if(n==1){
-            return sum;
+    public static void func1(int n){
+        if(n==0){
+            return;
         }
+        
+        func1(n-1);
+        sum = sum + n;
+
+        
+        
     }
 }
