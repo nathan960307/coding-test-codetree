b960580c61786db9437954411940fdd60da88d53
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-27 16:19:47 +0900

    Title: 재귀함수의 꽃, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

--- 251227/재귀함수의 꽃/the-flower-of-the-recursive-function.java
+++ 251227/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -7,15 +7,13 @@ public class Main {
         func1(n);
     }
     public static void func1(int n){
-        if(n>1){
-            System.out.print(n + " ");
-            func1(n-1);
-        }else if(n>=1){
-            func1(n-1);
-            System.out.print(n + " ");
-        }else{
+        if(n==0){
             return;
         }
+
+        System.out.print(n + " ");
+        func1(n-1);
+        System.out.print(n + " ");
         
     }
 }
