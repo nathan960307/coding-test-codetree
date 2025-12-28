94a7ce6976537911c306516355d96dd2b382ac08
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-28 17:03:52 +0900

    Title: Factorial, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 251228/Factorial/factorial.java
+++ 251228/Factorial/factorial.java
@@ -14,7 +14,7 @@ public class Main {
         }else if(n==1){
             return 1;
         }else if(n==2){
-            return 3;
+            return 2;
         }else if(n==3){
             return 6;
         }
