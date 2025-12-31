bfcab1fcaac423ff9a07e3cea6e01af2246fca8e
nathan <168330519+nathan960307@users.noreply.github.com>
2025-12-31 16:57:16 +0900

    Title: 사는 지역, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251231/사는 지역/where-live.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] names = new String[n];
+        String[] addresses = new String[n];
+        String[] regions = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            names[i] = sc.next();
+            addresses[i] = sc.next();
+            regions[i] = sc.next();
+        }
+        char min = 'a';
+        int index = 0;
+
+        // Please write your code here.
+        data[] datas = new data[n];
+
+        for(int i = 0; i < n; i++) {
+            datas[i] = new data(names[i],addresses[i],regions[i]);
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(datas[i].name.charAt(0) >= min){
+                min = datas[i].name.charAt(0);
+                index = i;
+            }
+        }
+
+        System.out.println("name " + datas[index].name);
+        System.out.println("addr " + datas[index].address);
+        System.out.println("city " + datas[index].region);
+    }
+}
+
+class data {
+    String name;
+    String address;
+    String region;
+
+    public data(String name, String address, String region){
+        this.name = name;
+        this.address = address;
+        this.region = region;
+    }
+};
