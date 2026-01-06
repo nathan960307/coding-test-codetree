db388125bd5fbd6683e998b4363ac9e1d99a21a8
nathan <168330519+nathan960307@users.noreply.github.com>
2026-01-07 00:44:55 +0900

    Title: 원점으로부터의 거리, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260107/원점으로부터의 거리/distance-from-origin.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] points = new int[n][2];
+        order[] orders = new order[n+1];
+        for (int i = 0; i < n; i++) {
+            points[i][0] = sc.nextInt();
+            points[i][1] = sc.nextInt();
+
+            int distance = Math.abs(points[i][0]) + Math.abs(points[i][1]);
+            orders[i+1] = new order(i+1,distance);
+        }
+        // Please write your code here.
+
+        Arrays.sort(order);
+        
+        for (order s : orders) {
+            System.out.printf("%d \n", s.num);
+        }
+    }
+}
+
+class order implements Comparable<order> {
+        int num;
+        int distance;
+
+        order(int num, int distance) {
+            this.num = num;
+            this.distance = weight;
+        }
+
+        @Override
+        public int compareTo(order o) {
+            if(this.distance == o.distance){
+                return this.num - o.num;
+            }
+            return o.distance - this.distance; 
+            
+        }
+};
