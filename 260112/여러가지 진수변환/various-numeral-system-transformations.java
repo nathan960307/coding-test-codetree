a866c6ef3c60b7d23aebe2af35eb089a1c3af842
nathan <168330519+nathan960307@users.noreply.github.com>
2026-01-12 15:37:48 +0900

    Title: 여러가지 진수변환, Time: 129ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260112/여러가지 진수변환/various-numeral-system-transformations.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 10진수인 정수
+        int b = sc.nextInt(); // 변경할 진수, 4 8 두 가지로 주어짐
+        // Please write your code here.
+        int[] arr = new int[10];
+        int count = 0;
+
+        int answer = 0;
+
+        while(true){
+            if(n == 1){
+                arr[count++] = n;
+                break;
+            }
+
+            arr[count++] = n % b;
+            n = n / b;
+            
+        }
+
+        for(int i = count - 1; i >= 0; i--){
+            System.out.print(arr[i]);
+        }
+            
+    }
+}
+
+
