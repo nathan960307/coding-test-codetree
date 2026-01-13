dbab35831f1d77e4bcb7bf63a07f9f0e4ac4bbde
nathan <168330519+nathan960307@users.noreply.github.com>
2026-01-13 09:53:02 +0900

    Title: 최대로 겹치는 구간, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 260113/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 260113/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -9,19 +9,19 @@ public class Main {
         int[] x1 = new int[n]; // 선분의 시작 점
         int[] x2 = new int[n]; // 선분의 끝 점
 
-        int[] arr = new int[100]; // 겹치는 구간 표시 배열
+        int OFFSET = 100; // 음수 보정
+        int[] arr = new int[201]; // 겹치는 구간 표시 배열
         for (int i = 0; i < n; i++) {
             x1[i] = sc.nextInt();
             x2[i] = sc.nextInt();
             for(int j=x1[i]; j<=x2[i]-1; j++){
-                arr[j-1] +=1;
+                arr[j+OFFSET] +=1;
             }
         }
 
-
         int max =0; 
 
-        for(int i=0; i<100; i++){
+        for(int i=0; i<201; i++){
             if(arr[i] > max){
                 max = arr[i];
             }
