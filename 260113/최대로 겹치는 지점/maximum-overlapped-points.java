b3516939988b6e83abd0f405ed212b05da9c1085
nathan <168330519+nathan960307@users.noreply.github.com>
2026-01-13 09:54:53 +0900

    Title: 최대로 겹치는 지점, Time: 131ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260113/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[100]; // 겹치는 지점 표시 배열
+        int[] start = new int[n];
+        int[] end = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            start[i] = sc.nextInt();
+            end[i] = sc.nextInt();
+            for(int j=start[i]; j<=end[i]; j++){
+                arr[j] +=1;
+            }
+        }
+
+        int max =0; 
+
+        for(int i=0; i<100; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+
+        System.out.print(max);
+    }
+}
