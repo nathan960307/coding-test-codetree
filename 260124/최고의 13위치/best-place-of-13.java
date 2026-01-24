d933a56a1a1370cb40ca9f15ba5faa348c86dce4
nathan <168330519+nathan960307@users.noreply.github.com>
2026-01-24 16:50:42 +0900

    Title: 최고의 13위치, Time: 157ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 260124/최고의 13위치/best-place-of-13.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int max = 0;
+        int n = sc.nextInt();
+        int[][] grid = new int[n][n];
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                grid[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-2; j++){
+                max = Math.max(max, grid[i][j]+grid[i][j+1]+grid[i][j+2]);
+            }
+        }
+        System.out.print(max);
+            
+    }
+}
