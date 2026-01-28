a745a3f34564ad111fc56f64c27058adf5daba78
nathan <168330519+nathan960307@users.noreply.github.com>
2026-01-28 12:22:53 +0900

    Title: 이상한 진수 2, Time: 115ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260128/이상한 진수 2/awkward-digits-2.java
+++ 260128/이상한 진수 2/awkward-digits-2.java
@@ -4,16 +4,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
         int len = a.length();
+        boolean changed = false;
 
         for(int i=0; i<len; i++){
             int temp = a.charAt(i);
             if(temp=='0'){
                 a = a.substring(0,i) + 1 + a.substring(i+1,len);
+                changed = true;
                 break;
             }
         }
 
         int decimal = Integer.parseInt(a, 2);
+
+        if(!changed){
+            a = a.substring(0, len - 1) + "0";
+        }
 
         System.out.print(decimal);
     }
