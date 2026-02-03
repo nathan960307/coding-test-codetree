186d990cf42f51dcee302e4617264b242217110a
nathan <168330519+nathan960307@users.noreply.github.com>
2026-02-03 12:30:37 +0900

    Title: 구간 중 최대 합, Time: 138ms, Memory: 10MB, Status: Passed - Codetree

--- 260203/구간 중 최대 합/max-sum-of-subarray.java
+++ 260203/구간 중 최대 합/max-sum-of-subarray.java
@@ -10,7 +10,7 @@ public class Main {
         // Please write your code here.
         int max = 0;
 
-        for(int i=0; i<n-k; i++){
+        for(int i=0; i<=n-k; i++){
             int sum = 0;
             for(int j=i; j<i+k; j++){
                 sum = sum + arr[j];
